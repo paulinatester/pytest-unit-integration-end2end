889a246f1631fe08aed693600c94609d47d8b4b8
Paulina <paulina@webso.be>
2018-02-11 13:47:03 +0100

    main file

---
+++ main.py
@@ -0,0 +1,18 @@
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    if b == 0:
+        raise ValueError("Cannot divide by zero")
+    return a / b
+
+# API Endpoint
+def api_endpoint(data):
+    # Some API logic here
+    return f"Processed: {data}"
